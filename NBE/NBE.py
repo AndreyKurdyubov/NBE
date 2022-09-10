136db4ebdaa5e6cf8bbd8e057eb7e20b96503aef
AndreyKurdyubov <a.s.kurdyubov@gmail.com>
2022-09-10 13:18:09 +0300

    Initial commit

---
+++ NBE/NBE.py
@@ -0,0 +1,17 @@
+def ivp(xdata, N0, k, gd, kex, kc, gnr, bgr):
+    '''
+    solve initial value problem 
+    '''
+    
+    def scatter(t, y, N0, k, gd, kex, kc, gnr):
+        nx_hot = ex_(N0*(1-k), gd, t)
+        n, nx = y
+        return [gd*nx_hot - kex*n**2 - gnr*n, 
+                kex*n**2 - kc*2*n*nx]
+    
+    n0 = [0, bgr + N0*k]
+    tspan = (0, xdata[-1])
+    fun = lambda t, y: scatter(t, y, N0, k, gd, kex, kc, gnr)
+    sol = solve_ivp(fun, tspan, n0, t_eval=xdata)
+    
+    return sol
